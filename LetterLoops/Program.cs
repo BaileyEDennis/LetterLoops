f4446914b3152e22efc020629bf0d777b70f4bf9
Bailey <bailey.elise.dennis@gmail.com>
2021-01-14 19:47:20 -0600

    finished letter loops

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some letters below:");
+            var letters = Console.ReadLine();
+            string output = string.Empty;
+            var count = 1;
+            foreach (char c in letters)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    if (i == 0)
+                    {
+                        output += char.ToUpper(c);
+                    }
+                    else
+                    {
+                        output += c;
+                    }
+                }
+                output += "-";
+                count++;
+            }
+            string trimmed = output.TrimEnd('-');
+            Console.WriteLine($"Here is the new output {trimmed}");
+
+        }
+    }
+}
